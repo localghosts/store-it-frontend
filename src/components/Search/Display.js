02931675f0c13c389525f9ca079f0a5cc1cbdd5d
ManasGupta1310 <mmanasggupta@gmail.com>
2022-02-18 14:02:18 +0530

    SIA-13 Add global search

---
+++ src/components/Search/Display.js
@@ -0,0 +1,26 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import Stack from '@mui/material/Stack';
+import Card from './Card';
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';  
+
+export default function Display({options, display}) {
+    
+  return display===true?(
+    <Stack direction="column" spacing={2} alignItems="center">
+        <div className='display-search'>
+        {options.map((item)=>(
+            <Card storeElement={item}/>
+        ))}
+        <Button size="small" 
+        sx={{
+            width:200,
+            fontSize:10,
+            borderRadius:10,
+        }}endIcon={<ArrowForwardIosIcon />}>
+            See more stores.....
+        </Button>
+        </div>
+    </Stack>
+  ):(<div></div>);
+}
