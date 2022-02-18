520bd8ed7bc1dc1c1bc8bb8f927b77c7914b32e8
Priya Gole <priyagole1607@gmail.com>
2022-02-18 17:03:18 +0530

    Added Navigation bar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Navbar from "./components/Navbar"
 import SearchField from "./components/Search/SearchField"
 
 function App() {
   return (
-    <SearchField/>
+    <SearchField />,
+    <Navbar />
   );
 }
 
