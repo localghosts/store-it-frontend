c74b0d91ebe2825fe72c5f03d29e0840f3de7674
Manas Gupta <mmanasggupta@gmail.com>
2022-03-04 19:11:15 +0530

    Add state for catagories also

--- src/components/SellerView/Dashboard/Dashboard.js
+++ src/components/SellerView/Dashboard/Dashboard.js
@@ -3,6 +3,7 @@ import Navigation from './Navigation/Navigation'
 import Products from './Products/Products'
 import "./Dashboard.css"
 import { useParams } from 'react-router-dom'
+// import Categories from './Categories/Categories'
 
 const Dashboard = () => {
   
@@ -27,6 +28,27 @@ const Dashboard = () => {
     }
   ])
 
+  const [categories, setCategories] = useState([
+    {
+      title: "Pizza",
+      description: "A wide variety of mouth-watering pizzas in three categories : small, medium and large.",
+      img: "https://images.unsplash.com/photo-1513104890138-7c749659a591?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cGl6emF8ZW58MHwwfDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+      status: true
+    },
+    {
+      title: "Extras",
+      description: "Breadsticks, dips, desserts and many more to compliment your delicious Pizza meal !!",
+      img: "https://images.unsplash.com/photo-1469648034646-7911874fe62b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8YXJsaWMlMjBicmVhZHxlbnwwfDB8MHx8&auto=format&fit=crop&w=500&q=60",
+      status: true
+    },
+    {
+      title: "Beverages",
+      description: "A wide range of hot and cold beverages to complete your perfect meal.",
+      img: "https://images.unsplash.com/photo-1495474472287-4d71bcdd2085?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8YmV2ZXJhZ2VzfGVufDB8MHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
+      status: true
+    }
+  ])
+
   return (
     <div className='dashboard'>
         <div className='navigation'>
@@ -35,6 +57,7 @@ const Dashboard = () => {
 
         <div>
           {(active==="products")?<Products active={active} setActive={setActive} products={products} setProducts={setProducts}/>:<></>}
+          {/* {(active==="categories")?<Categories active={active} setActive={setActive} categories={categories} setCategories={setCategories}/>:<></>} */}
         </div>
 
     </div>
