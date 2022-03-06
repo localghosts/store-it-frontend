74917c412d4cd7388fdf864836d98941bed739b2
Manas Gupta <mmanasggupta@gmail.com>
2022-03-06 10:58:41 +0530

    Add form validations and checkout address

--- src/components/BuyerView/Store/StoreBill/StoreBill.js
+++ src/components/BuyerView/Store/StoreBill/StoreBill.js
@@ -7,10 +7,59 @@ import Typography from '@mui/material/Typography';
 import { grey } from '@mui/material/colors';
 import "./StoreBill.css"
 import { Button } from '@mui/material';
-
+import { useState } from 'react';
+import { TextField } from '@mui/material';
 
 export default function StoreBill() {
   
+  const [cart, setCart]=useState([
+    [
+      {
+        item: "Big Mac",
+        qty: 2,
+        price: 50
+      },
+      {
+        item: "Pepsi",
+        qty: 2,
+        price: 50
+      },
+      {
+        item: "Mc Aloo Tikki",
+        qty: 1,
+        price: 40
+      },
+    ],
+    {
+      total: 240
+    }
+  ])
+  const [checkOut, setCheckOut]=useState(false);
+  const [address, setAddress]=useState("");
+  const [errorAddress, setErrorAddress]=useState(false);
+
+  const handleCheckOut=()=>{
+    setCheckOut(true);
+  }
+  
+  const fieldValidation=(address)=>{
+    if(!address){
+      return false;
+    }
+    return true;
+  }
+
+  const handleSubmit=()=>{
+    if(!fieldValidation(address)){
+      setErrorAddress(true)
+    }
+    else{
+      setCheckOut(false)
+      setAddress("")
+      setErrorAddress(false)
+    }
+  }
+
   return (
     <Card sx={{ width: "17vw", borderRadius: 10 ,backgroundColor:grey[200]}}>
       <CardHeader title="Cart" sx={{textDecoration:"underline"}}/>
@@ -18,31 +67,50 @@ export default function StoreBill() {
       <CardContent>
         <Typography variant="body2" color="text.primary">
         <div className='store-bill'>
-          <div className='storeitems-card'>
-            <div className='item'>Pizza</div>
-            <div className='item'>Ice-cream</div>
-          </div>
-          <div className='storeprice-card'>
-            <div className='price'>Rs 100</div>
-            <div className='price'>Rs 100</div>
-          </div>
+          {cart[0].map((cartItem)=>(
+            <div className='storeBillItem'>
+              <div className='storeitems-card'>
+                <div className='item'>{cartItem.item}</div>
+              </div>
+              <div className='storeqty-card'>
+                <div className='itemqty'>{cartItem.qty} x</div>
+              </div>
+              <div className='storeprice-card'>
+                <div className='price'>Rs {cartItem.price}</div>
+              </div>
+            </div>
+            ))}
         </div>
         <div className='store-bill-total'>
           <div className='storeitems-total'>
             <div className='item'>Total</div>
           </div>
           <div className='storeprice-total'>
-            <div className='price'>Rs 200</div>
+            <div className='price'>Rs {cart[1].total}</div>
           </div>
         </div>
         </Typography>
         <Typography>
             <div className='checkout'>
-                <Button variant='contained' 
+              {(checkOut===false)?<Button variant='contained' 
                 sx={{borderRadius: 10, width: "100%"}}
-                startIcon={<ShoppingCartIcon/>}>CheckOut</Button>
+                startIcon={<ShoppingCartIcon/>} onClick={handleCheckOut}>CheckOut</Button>
+              :<></>}
             </div>
         </Typography>
+        <div className='addressForm'>
+          {(checkOut===true)?
+              (<div className='addProductForm'>
+                  <div className='form-component address-field'>
+                    <TextField required id="outlined-required" label="Address" sx={{width:280}}  value={address} 
+                    onChange={(e)=>setAddress(e.target.value)} error={errorAddress} helperText={errorAddress===true?"Missing entry":""} />
+                  </div>
+                  <div className='form-component submit-btn'>
+                    <Button variant="contained" size="large" sx={{width:"200px", borderRadius: 10}} onClick={handleSubmit}>Submit</Button>
+                  </div>
+              </div>)
+              :<></>}
+        </div>
       </CardContent>
     </Card>
   );
--- src/components/SellerView/Dashboard/Products/AddProducts/AddProduct.js
+++ src/components/SellerView/Dashboard/Products/AddProducts/AddProduct.js
@@ -13,28 +13,43 @@ export default function AddProduct({products, setProducts}) {
   const [name, setName]=useState("");
   const [category, setCategory]=useState("");
   const [price, setPrice]=useState("");
+  const [errorName, setErrorName]=useState(false);
+  const [errorCategory, setErrorCategory]=useState(false);
+  const [errorPrice, setErrorPrice]=useState(false);
 
   const fieldValidation=(name, category, price)=>{
     if(name==="" || category==="" || price===""){
+      if(name==="") setErrorName(true);
+      else  setErrorName(false)
+
+      if(category==="") setErrorCategory(true);
+      else  setErrorCategory(false)
+
+      if(price==="") setErrorPrice(true);
+      else  setErrorPrice(false)
+      
       return false;
     }
+    setErrorName(false)
+    setErrorCategory(false)
+    setErrorPrice(false)
     return true;
   }
 
   const priceValidation=(price)=>{
     if(isNaN(price)){
+      setErrorPrice(true);
       return false;
+    }
+    else{
+      setErrorPrice(false);
     }
     return true;
   }
 
   const handleSubmit=()=>{
-    if(!fieldValidation(name, category, price)){
-      alert("Fill in the required fields")
-    }
-    else if(!(priceValidation(price))){
-      alert("Price is not a number")
-    }
+    if(!fieldValidation(name, category, price));
+    else if(!(priceValidation(price)));
     else{
       const product={
         "product":name,
@@ -55,16 +70,20 @@ export default function AddProduct({products, setProducts}) {
         <CardContent>
           <form>
               <div className='addProductForm'>
-              <div className='form-component form-title'></div>
+              <div className='form-component form-title'>
                 <Typography><h1>Add a product</h1></Typography>
+              </div>
               <div className='form-component category-field'>
-                <TextField required id="outlined-required" label="Product" sx={{width:300}} value={name} onChange={(e)=>setName(e.target.value)}/>
+                <TextField required id="outlined-required" label="Product" sx={{width:300}} value={name} 
+                onChange={(e)=>setName(e.target.value)} error={errorName} helperText={errorName===true?"Missing entry":""}/>
               </div>
               <div className='form-component product-field'>
-                <TextField required id="outlined-required" label="Category" sx={{width:300}} value={category} onChange={(e)=>setCategory(e.target.value)}/>
+                <TextField required id="outlined-required" label="Category" sx={{width:300}} value={category} 
+                onChange={(e)=>setCategory(e.target.value)} error={errorCategory} helperText={errorCategory===true?"Missing entry":""}/>
               </div>
               <div className='form-component price-field'>
-                <TextField required id="outlined-required" label="Price" sx={{width:300}} value={price} onChange={(e)=>setPrice(e.target.value)}/>
+                <TextField required id="outlined-required" label="Price" sx={{width:300}}  value={price} 
+                onChange={(e)=>setPrice(e.target.value)} error={errorPrice} helperText={errorPrice===true?"Invalid entry. Price should be a number":""}/>
               </div>
               <div className='form-component submit-btn'>
                 <Button variant="contained" size="large" sx={{width:"200px"}} onClick={handleSubmit}>Submit</Button>
