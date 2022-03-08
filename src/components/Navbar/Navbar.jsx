475f2c4b803afc8541b8f18ca7483a97800578a3
Manas Gupta <mmanasggupta@gmail.com>
2022-03-08 21:59:26 +0530

    Fix lint errors

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
-import Display from './Display.js';
+import Display from './Display';
 import { styled, alpha } from '@mui/material/styles';
 import InputBase from '@mui/material/InputBase';
 import SearchIcon from '@mui/icons-material/Search';
--- src/components/SellerView/Dashboard/Products/Products.jsx
+++ src/components/SellerView/Dashboard/Products/Products.jsx
@@ -1,20 +1,18 @@
-import React from 'react'
-import AddProduct from "./AddProducts/AddProduct"
-import ProductLog from "./ProductLog/ProductLog" 
-import "./Products.css"
+import React from 'react';
+import AddProduct from './AddProducts/AddProduct';
+import ProductLog from './ProductLog/ProductLog';
+import './Products.css';
 
-const Products = ({products, setProducts}) => {
+function Products({ products, setProducts }) {
   return (
-    <div className='product'>
-        <div>
-          <ProductLog products={products} setProducts={setProducts}/>
-        </div>
-        
-        <div>
-          <AddProduct products={products} setProducts={setProducts}/>
-        </div>
+    <div className="product">
+      <div>
+        <ProductLog products={products} setProducts={setProducts} />
+      </div>
+      <div>
+        <AddProduct products={products} setProducts={setProducts} />
+      </div>
     </div>
-  )
+  );
 }
-
-export default Products
+export default Products;
