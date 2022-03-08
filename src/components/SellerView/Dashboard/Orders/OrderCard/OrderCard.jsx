a9b9af47abffba36f9f9eeaf39e3988098480f3c
Bhuvan Singla <bhuvansingla2000@gmail.com>
2022-03-09 02:35:44 +0530

    Lint

--- src/components/SellerView/Dashboard/Orders/OrderCard/OrderCard.jsx
+++ src/components/SellerView/Dashboard/Orders/OrderCard/OrderCard.jsx
@@ -7,102 +7,128 @@ import CardContent from '@mui/material/CardContent';
 import Avatar from '@mui/material/Avatar';
 import Typography from '@mui/material/Typography';
 import { grey, red } from '@mui/material/colors';
-import "./OrderCard.css"
+import './OrderCard.css';
 import { useState } from 'react';
 
 export default function OrderCard({ history }) {
-      
-  const AfterRejected = () => {
-    return <div>
-      <div className='center'>
-        <b>ORDER REJECTED </b>
+  function AfterRejected() {
+    return (
+      <div>
+        <div className="center">
+          <b>ORDER REJECTED </b>
+        </div>
       </div>
-      </div>
+    );
   }
 
-  const AfterAccepted = () => {
-    return <div>
-      <div className='orderAcceptedText'>
-        <b>ORDER ACCEPTED</b>
+  function AfterAccepted() {
+    return (
+      <div>
+        <div className="orderAcceptedText">
+          <b>ORDER ACCEPTED</b>
+        </div>
+        <TextField label="Update status" size="small" variant="outlined" style={{ marginRight: '10px' }} />
+        <div className="buttons">
+          <Button variant="contained" onClick={() => { }}> Update </Button>
+        </div>
       </div>
-      <TextField label="Update status" size="small" variant="outlined" style={{marginRight:'10px'}} />
-      <div className='buttons'>
-        <Button variant={"contained"} onClick={() => { }}> Update </Button>
-      </div>
-      </div>
+    );
   }
 
-  const AcceptOrReject = () => {
-    return <div className = "buttons">
-      <Button variant={"contained"} onClick={() => setAccepted(true)}> Accept </Button>
-      <Button variant={"contained"} onClick={() => setRejected(true)}> Reject </Button>
-    </div>
+  function AcceptOrReject() {
+    return (
+      <div className="buttons">
+        <Button variant="contained" onClick={() => setAccepted(true)}> Accept </Button>
+        <Button variant="contained" onClick={() => setRejected(true)}> Reject </Button>
+      </div>
+    );
   }
 
-  const [accepted,setAccepted]=useState(false)
-  const [rejected, setRejected] =useState(false)
+  const [accepted, setAccepted] = useState(false);
+  const [rejected, setRejected] = useState(false);
 
   return (
-    <div  className="card">
-    <Card sx={{ maxWidth: 400, borderRadius: 3, backgroundColor: grey[200]}}>
-      <CardHeader
-        avatar={
-          <Avatar sx={{ bgcolor: red[300], height:80, width:80 }} src={history["userProfile"]}/>
+    <div className="card">
+      <Card sx={{ maxWidth: 400, borderRadius: 3, backgroundColor: grey[200] }}>
+        <CardHeader
+          avatar={
+            <Avatar sx={{ bgcolor: red[300], height: 80, width: 80 }} src={history.userProfile} />
         }
-        title={<span className='orderTitle'>
-          <b><h2>Order # {history["orderNumber"]}</h2></b>
-          <span> </span>
-        </span>}
-        subheader={<span>
-          <b><h3>{history["orderTime"]}</h3></b>
-        </span>}
+          title={(
+            <span className="orderTitle">
+              <b>
+                <h2>
+                  Order #
+                  {history.orderNumber}
+                </h2>
+              </b>
+              <span> </span>
+            </span>
+)}
+          subheader={(
+            <span>
+              <b><h3>{history.orderTime}</h3></b>
+            </span>
+)}
         />
 
-      <CardContent>
-        <Typography variant="body2" color="text.primary" sx={{fontWeight: "bold", fontFamily:"Arial"}}>
-          <div className='bill'>
-            {history["products"].map((product,index)=>(
-              <div className='bill-item'>
-                <div className='items-card'>
-                  <div className='item'>{product["name"]}</div>
+        <CardContent>
+          <Typography variant="body2" color="text.primary" sx={{ fontWeight: 'bold', fontFamily: 'Arial' }}>
+            <div className="bill">
+              {history.products.map((product, index) => (
+                <div className="bill-item">
+                  <div className="items-card">
+                    <div className="item">{product.name}</div>
+                  </div>
+                  <div className="items-quantity">
+                    <div className="item">
+                      {product.quantity}
+                      {' '}
+                      x
+                      {' '}
+                    </div>
+                  </div>
+                  <div className="price-card">
+                    <div className="item">
+                      {product.price === 1 ? 'Re.' : 'Rs.'}
+                      {' '}
+                      {product.price}
+                    </div>
+                  </div>
                 </div>
-                <div className='items-quantity'>
-                  <div className='item'>{product["quantity"]} x </div>
+              ))}
+            </div>
+            <div className="bill-total">
+              <div className="items-total">
+                <div className="item">Total</div>
+              </div>
+              <div className="price-total">
+                <div className="price">
+                  Rs.
+                  {history.total}
                 </div>
-                <div className='price-card'>
-                  <div className='item'>{product["price"]===1?"Re.":"Rs."} {product["price"]}</div>
               </div>
-              </div>
-            ))}
-          </div>
-        <div className='bill-total'>
-          <div className='items-total'>
-            <div className='item'>Total</div>
-          </div>
-          <div className='price-total'>
-            <div className='price'>Rs. {history["total"]}</div>
-          </div>
-        </div>
-        </Typography>
-      </CardContent>
-      <CardContent>
-      <Typography variant="body2" color="text.primary" sx={{fontWeight: "bold"}}>Name:</Typography>
-        <Typography paragraph variant="body2" color="text.secondary">
-          {history["buyer"]}
-        </Typography>
-
-        <Typography variant="body2" color="text.primary" sx={{fontWeight: "bold"}}>Address:</Typography>
-        <Typography paragraph variant="body2" color="text.secondary">
-          {history["address"]}
-        </Typography>
-
-        <Typography  variant="body2" color="text.primary" sx={{fontWeight: "bold"}}>Payment Status:</Typography>
-        <Typography paragraph variant="body2" color="text.secondary">{history["paidBy"]}</Typography>
-      </CardContent>
-        <CardContent className='bordertop'>
-          {(accepted===true || rejected===true)?(accepted===true)?<AfterAccepted/>:<AfterRejected/>:<AcceptOrReject />}
+            </div>
+          </Typography>
         </CardContent>
-    </Card>
+        <CardContent>
+          <Typography variant="body2" color="text.primary" sx={{ fontWeight: 'bold' }}>Name:</Typography>
+          <Typography paragraph variant="body2" color="text.secondary">
+            {history.buyer}
+          </Typography>
+
+          <Typography variant="body2" color="text.primary" sx={{ fontWeight: 'bold' }}>Address:</Typography>
+          <Typography paragraph variant="body2" color="text.secondary">
+            {history.address}
+          </Typography>
+
+          <Typography variant="body2" color="text.primary" sx={{ fontWeight: 'bold' }}>Payment Status:</Typography>
+          <Typography paragraph variant="body2" color="text.secondary">{history.paidBy}</Typography>
+        </CardContent>
+        <CardContent className="bordertop">
+          {(accepted === true || rejected === true) ? (accepted === true) ? <AfterAccepted /> : <AfterRejected /> : <AcceptOrReject />}
+        </CardContent>
+      </Card>
     </div>
   );
 }
