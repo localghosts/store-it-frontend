d8064db1a5ef17712a89b10819643b5e5c643fbc
Manas Gupta <mmanasggupta@gmail.com>
2022-03-19 00:06:00 +0530

    Fix error handling

--- src/components/Login-SignUp/Seller/SellerLogin.jsx
+++ src/components/Login-SignUp/Seller/SellerLogin.jsx
@@ -19,7 +19,7 @@ function SellerLogin({ setAuth }) {
   const [errorEmail, setErrorEmail] = useState(false);
   const [errorPass, setErrorPass] = useState(false);
   const [loginError, setLoginError] = useState(false);
-
+  const [errorMsg, setErrorMsg] = useState('');
   // Loading States
   const [loading, setLoading] = useState(false);
 
@@ -68,6 +68,7 @@ function SellerLogin({ setAuth }) {
         setAuth(true);
       })
       .catch(((err) => {
+        setErrorMsg(err.response.data.message);
         status = err?.response?.status ?? 500;
         setLoginError(true);
         setLoading(false);
@@ -100,7 +101,7 @@ function SellerLogin({ setAuth }) {
                 )}
                 sx={{ mb: 2 }}
               >
-                Incorrect Email or Password!
+                {errorMsg}
               </Alert>
             </div>
           </Collapse>
--- src/components/SellerView/Dashboard/Navigation/Navigation.jsx
+++ src/components/SellerView/Dashboard/Navigation/Navigation.jsx
@@ -1,10 +1,14 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import Button from '@mui/material/Button';
 import './Navigation.css';
 import { Link, useNavigate } from 'react-router-dom';
 import { red } from '@mui/material/colors';
+import axios from 'axios';
+import { Typography } from '@mui/material';
+import BASE_URL from '../../../../url';
 
 function Navigation({ active, setActive, storeSlug }) {
+  const [storeInfo, setStoreInfo] = useState({});
   const handleType = (type) => {
     setActive(type);
   };
@@ -14,9 +18,23 @@ function Navigation({ active, setActive, storeSlug }) {
     if (!localStorage.getItem('token')) {
       navigate('/login');
     }
-  }, []);
+    const config = {
+      headers: {
+        Authorization: localStorage.getItem('token'),
+      },
+    };
+    axios
+      .get(`${BASE_URL}/store/${storeSlug}`, config)
+      .then((res) => setStoreInfo(res.data.store))
+      .catch((err) => console.log(err));
+  }, [storeSlug]);
+
   return (
     <div className="navigation">
+      <div className="navStoreLogo">
+        <img src={storeInfo.storelogo} alt={storeSlug} height={100} />
+        <Typography sx={{ fontSize: 30, fontWeight: 'bold', margin: '25px 0px' }}>{storeInfo.storename}</Typography>
+      </div>
       <div className="order-nav nav-btn">
         <Link to={`/seller/${storeSlug}/dashboard/orders`} style={{ textDecoration: 'none', color: red[50] }}>
           <Button
