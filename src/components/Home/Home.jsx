b74faadd6a79ae5bec2f3521420994a5d51c8927
Manas Gupta <mmanasggupta@gmail.com>
2022-03-20 19:07:46 +0530

    Fix store tile spacing

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -5,7 +5,6 @@ import Catalog from './Catalog/Catalog';
 function Home() {
   return (
     <div className="content">
-      <div className="contentBanner" />
       <div className="catalogArea">
         <Catalog />
       </div>
